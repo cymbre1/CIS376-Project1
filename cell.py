32457ecbd09595d3d657aec2bb941a28fc6d66ba
Cymbre Spoehr <cymbre@mattscott.org>
2023-01-27 16:48:01 -0500

    I did some stuff

---
+++ cell.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+class Cell:
+    color = (0,0,0)
+
+
+    def onClick():
+        # this will do somthing on a click
+        print("This will do something")
---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+data = []
+
+while True:
+    # process inputs
+    # update objects
+    # process outputs
+    print("Hello")

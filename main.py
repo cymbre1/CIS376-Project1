bd5080f5c4a4110077e65ca1cc1c5c52bb1a2979
Cymbre Spoehr <cymbre@mattscott.org>
2023-02-01 09:06:34 -0500

    So much refactoring

--- main.py
+++ main.py
@@ -12,27 +12,56 @@ class Board():
         self.squares = [[Square() for j in range(self.cols)] for i in range(self.rows)]
 
 
-    def generate_maze(squares, rows, cols):
+    def generate_maze(self):
         alive = 0
-        for x in range(rows):
-            for y in range(cols):
-                if squares[x][y].color != (0,0,0):
+        for x in range(self.rows):
+            for y in range(self.cols):
+                if self.squares[x][y].color != (0,0,0):
                     alive += 1
-                num_living = get_num_living(squares, find_neighbors((x,y)))
+                num_living = get_num_living(self.squares, self.find_neighbors((x,y)))
                 if num_living == 3:
-                    squares[x][y].born()
+                    self.squares[x][y].born()
                 if num_living < 1 or num_living > 3:
-                    squares[x][y].die()
+                    self.squares[x][y].die()
         return alive
 
-    def gen_random_seed(squares, rows, cols):
+    def gen_random_seed(self):
         distance = random.randint(0, 5)
-        for x in range(rows):
-            for y in range (cols):
+        for square_row in self.squares:
+            for square in square_row:
                 if distance == 0:
                     distance = random.randint(1, 6)
-                    squares[x][y].born()
+                    square.born()
                 distance -= 1
+
+    def find_neighbors(self, index):
+        neighbors = []
+        right = index[0] + 1
+        left = index[0] - 1
+        up = index[1] + 1
+        down = index[1] - 1
+        if is_valid_index((index[0], up)): neighbors.append((index[0], up))
+        if is_valid_index((right, up)): neighbors.append((right, up))
+        if is_valid_index((left, up)): neighbors.append((left, up))
+        if is_valid_index((index[0], down)): neighbors.append((index[0], down))
+        if is_valid_index((right, down)): neighbors.append((right, down))
+        if is_valid_index((left, down)): neighbors.append((left, down))
+        if is_valid_index((right, index[1])): neighbors.append((right, index[1]))
+        if is_valid_index((left, index[1])): neighbors.append((left, index[1]))
+        return neighbors
+
+def get_num_living(self, squares, neighbors):
+    num_living = 0
+    for n in neighbors:
+        if(squares[n[0]][n[1]].color != (0,0,0)):
+            num_living = num_living + 1
+    return num_living
+
+def reset(self, squares, rows, cols, playerToken):
+    for x in range(rows):
+        for y in range(cols):
+            squares[x][y].die()
+    playerToken.reset_position()
 
 class Square(pygame.sprite.DirtySprite):
     color = ()
@@ -126,13 +155,13 @@ class Engine():
         self.FRAMERATE = 60
         self.frameMili = 1000//self.FRAMERATE
 
-        self.squares = Board().squares
+        self.board = Board()
         self.token = Player()
 
         self.numAlive = 0
         self.is_generating_maze = False
 
-    def start_game(gameOn):
+    def start_game(self, gameOn):
         # Process inputs
         while gameOn:
             elapsed = pygame.time.get_ticks()
@@ -143,41 +172,41 @@ class Engine():
                     if event.key == K_g:
                         is_generating_maze = True
                     if event.key == K_r:
-                        gen_random_seed(squares, rows, cols)
+                        self.board.squares.gen_random_seed(self.board, self.board.rows, self.board.cols)
                     if event.key == K_UP or event.key == K_w:
-                        self.token.update(squares, rows, cols, 0, -36)
+                        self.token.update(self.board.squares, self.board.rows, self.board.cols, 0, -36)
                     if event.key == K_DOWN or event.key == K_s:
-                        self.token.update(squares, rows, cols, 0, 36)
+                        self.token.update(self.board.squares, self.board.rows, self.board.cols, 0, 36)
                     if event.key == K_RIGHT or event.key == K_d:
-                        self.token.update(squares, rows, cols, 36, 0)
+                        self.token.update(self.board.squares, self.board.rows, self.board.cols, 36, 0)
                     if event.key == K_LEFT or event.key == K_a:
-                        self.token.update(squares, rows, cols, -36, 0)
+                        self.token.update(self.board.squares, self.board.rows, self.board.cols, -36, 0)
                 if event.type == MOUSEBUTTONUP:
                     x = pygame.mouse.get_pos()[0] // 36
                     y = pygame.mouse.get_pos()[1] // 36
                     if x < 20 and y < 20:
-                        squares[x][y].update()
+                        self.board.squares[x][y].update()
                 elif event.type == QUIT:
                     gameOn = False
 
             if is_generating_maze:
-                newAlive = generate_maze(squares, rows, cols)
+                newAlive = self.board.generate_maze()
                 if newAlive == numAlive:
                     is_generating_maze = False
                 numAlive = newAlive
-                squares[0][0].die()
-                token.reset_position()
+                self.board.squares[0][0].die()
+                self.token.reset_position()
 
-            for i in range(rows):
-                for j in range(cols):
-                    if squares[i][j].dirty == 1:
-                        screen.blit(squares[i][j].surf, (i * 36, j * 36))
-            screen.blit(token.surf, (token.x, token.y))
+            for i in range(self.board.rows):
+                for j in range(self.board.cols):
+                    if self.board.squares[i][j].dirty == 1:
+                        screen.blit(self.board.squares[i][j].surf, (i * 36, j * 36))
+            screen.blit(self.token.surf, (self.token.x, self.token.y))
 
             pygame.display.flip()
 
-            delta = pygame.time.get_ticks() - elapsed
-            difference = frameMili - delta
+            self.delta = pygame.time.get_ticks() - elapsed
+            difference = self.frameMili - self.delta
             pygame.time.delay(difference)
 
 
@@ -186,86 +215,6 @@ class Engine():
 # tuple index, which represents the x,y coordinates for the desired index.
 def is_valid_index(index):
     return index[0] > -1 and index[0] < 20 and index[1] > -1 and index[1] < 20 
-
-def find_neighbors(index):
-    neighbors = []
-    right = index[0] + 1
-    left = index[0] - 1
-    up = index[1] + 1
-    down = index[1] - 1
-    if is_valid_index((index[0], up)): neighbors.append((index[0], up))
-    if is_valid_index((right, up)): neighbors.append((right, up))
-    if is_valid_index((left, up)): neighbors.append((left, up))
-    if is_valid_index((index[0], down)): neighbors.append((index[0], down))
-    if is_valid_index((right, down)): neighbors.append((right, down))
-    if is_valid_index((left, down)): neighbors.append((left, down))
-    if is_valid_index((right, index[1])): neighbors.append((right, index[1]))
-    if is_valid_index((left, index[1])): neighbors.append((left, index[1]))
-    return neighbors
-
-def get_num_living(squares, neighbors):
-    num_living = 0
-    for n in neighbors:
-        if(squares[n[0]][n[1]].color != (0,0,0)):
-            num_living = num_living + 1
-    return num_living
-
-def reset(squares, rows, cols, playerToken):
-    for x in range(rows):
-        for y in range(cols):
-            squares[x][y].die()
-    playerToken.reset_position()
-
-def start_game(gameOn):
-    # Process inputs
-
-
-    while gameOn:
-        elapsed = pygame.time.get_ticks()
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_BACKSPACE:
-                    gameOn = False
-                if event.key == K_g:
-                    is_generating_maze = True
-                if event.key == K_r:
-                    gen_random_seed(squares, rows, cols)
-                if event.key == K_UP or event.key == K_w:
-                    token.update(squares, rows, cols, 0,-36)
-                if event.key == K_DOWN or event.key == K_s:
-                    token.update(squares, rows, cols, 0,36)
-                if event.key == K_RIGHT or event.key == K_d:
-                    token.update(squares, rows, cols, 36,0)
-                if event.key == K_LEFT or event.key == K_a:
-                    token.update(squares,  rows, cols, -36,0)
-            if event.type == MOUSEBUTTONUP:
-                x = pygame.mouse.get_pos()[0] // 36
-                y = pygame.mouse.get_pos()[1] // 36
-                if x < 20 and y < 20:
-                    squares[x][y].update()
-            elif event.type == QUIT:
-                gameOn = False
-
-        if is_generating_maze:
-            newAlive = generate_maze(squares, rows, cols)
-            if newAlive == numAlive:
-                is_generating_maze = False
-            numAlive = newAlive
-            squares[0][0].die()
-            token.reset_position()
-
-        for i in range(rows):
-            for j in range(cols):
-                if squares[i][j].dirty == 1:
-                    screen.blit(squares[i][j].surf, (i*36, j*36))
-        screen.blit(token.surf, (token.x, token.y))
-
-        
-        pygame.display.flip()
-
-        delta = pygame.time.get_ticks() - elapsed
-        difference = frameMili - delta
-        pygame.time.delay(difference)
 
 
 pygame.init()

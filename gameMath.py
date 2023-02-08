4ba0fb92bb63755409b5a9a8da04d84c6c95996b
krbazner <krbazner@gmail.com>
2023-02-08 16:10:05 -0500

    skeleton code for math

--- gameMath.py
+++ gameMath.py
@@ -1,2 +1,49 @@
 class GameMath():
-    
+
+    #determine if two vectors have the same values
+    def v_is_equal(self, v1, v2):
+        return
+
+    #find the magnitude of a vector
+    def magnitude(self, v1):
+        return
+
+    #normalize a vector
+    def normalize(self, v):
+        return
+
+    #add two vectors
+    def v_add(self, v1, v2):
+        return
+
+    #subtract two vectors
+    def v_sub(self, v1, v2):
+        return
+
+    #calculate the angle between two vectors
+    def find_angle(self, v1, v2):
+        return
+
+    #determine if two matrices are the same
+    def m_is_equal(self, m1, m2):
+        return
+
+    #multipy a matrix by a matrix.  Return the matrix
+    def m_multiply(self, m1, m2):
+        return
+
+    #multiply a vector by a matrix and return the vector
+    def cross_multiply(self, v, m):
+        return
+
+    #add matrices and return a matrix
+    def m_add(self, m1, m2):
+        return
+
+    #subtract matrices and return a matrix
+    def m_sub(self, m1, m2):
+        return
+
+    #Data structure for 4x4 matrices
+    class Matrix():
+        # 2 dimensional array
